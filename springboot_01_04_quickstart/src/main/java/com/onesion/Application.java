4577fa54cf54286e0957480fe30cb8218b37980c
onesion <onesioner@163.com>
2025-03-27 23:09:25 +0800

    基于手工创建springboot入门案例

---
+++ springboot_01_04_quickstart/src/main/java/com/onesion/Application.java
@@ -0,0 +1,17 @@
+package com.onesion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author onesion
+ * @version 1.0
+ * @description TODO
+ * @date 2025/3/27 23:03
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
