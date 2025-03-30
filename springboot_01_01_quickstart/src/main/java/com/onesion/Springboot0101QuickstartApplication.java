7e2592609f485a5d95de88b27766fc936c8c5c53
onesion <onesioner@163.com>
2025-03-30 15:21:19 +0800

    演示main方法启动spring容器，定义的bean就可以加载到这里

--- springboot_01_01_quickstart/src/main/java/com/onesion/Springboot0101QuickstartApplication.java
+++ springboot_01_01_quickstart/src/main/java/com/onesion/Springboot0101QuickstartApplication.java
@@ -1,13 +1,22 @@
 package com.onesion;
 
+import com.onesion.controller.BookController;
+import com.onesion.controller.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Springboot0101QuickstartApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot0101QuickstartApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(Springboot0101QuickstartApplication.class, args);
+
+        BookController bean = ctx.getBean(BookController.class);
+        System.out.println("bean==========>" + bean);
+
+        User user = ctx.getBean(User.class);
+        System.out.println(user);
     }
 
 }
---
+++ springboot_01_01_quickstart/src/main/java/com/onesion/controller/User.java
@@ -0,0 +1,13 @@
+package com.onesion.controller;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author onesion
+ * @version 1.0
+ * @description TODO
+ * @date 2025/3/30 15:19
+ */
+@Component
+public class User {
+}
